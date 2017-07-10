a37ad21502c157e4d13ebb16332787a7efb83736
Axel <axelderomblay@gmail.com>
2017-07-10 10:49:15 +0200

    Update __init__.py

--- python-package/mlbox/__init__.py
+++ python-package/mlbox/__init__.py
@@ -5,7 +5,7 @@ __email__ = 'axelderomblay@gmail.com'
 __version__ = '0.2.2'
 
 from .preprocessing import *
-from .encoding import *
-from .optimisation import *
-from .prediction import *
-from .model import *
+#from .encoding import *
+#from .optimisation import *
+#from .prediction import *
+#from .model import *
